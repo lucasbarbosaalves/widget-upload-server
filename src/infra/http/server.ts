7c7974c97e6e0e5ba5c3f4c0bcbefcf616e4e384
lucasb <lkab05@hotmail.com>
2025-09-08 20:47:57 -0300

    env validation with zod

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -1,3 +1,4 @@
+import { env } from '@/env';
 import fastifyCors from '@fastify/cors';
 import { log } from 'console';
 import fastify from 'fastify';
@@ -5,6 +6,8 @@ import fastify from 'fastify';
 const server = fastify();
 
 server.register(fastifyCors, { origin: '*' });
+
+log(env.DATABASE_URL)
 
 server.listen({ port: 3000, host: '0.0.0.0' }).then(() => {
   log('HTTP Server Running on');
