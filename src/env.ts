e7b2e70eb12980c5310cc9ebb2b503cecdb95ef5
lucasb <lkab05@hotmail.com>
2025-09-08 21:55:47 -0300

    chore: swagger, multipart and drizzle deps

--- src/env.ts
+++ src/env.ts
@@ -1,9 +1,9 @@
-import z from "zod";
+import z from 'zod';
 
 const envSchema = z.object({
-  PORT: z.coerce.number().default(3333),
+  PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['development', 'test', 'production']),
-  DATABASE_URL: z.url().startsWith("postgresql://"),
-})
+  DATABASE_URL: z.url().startsWith('postgresql://'),
+});
 
 export const env = envSchema.parse(process.env);
