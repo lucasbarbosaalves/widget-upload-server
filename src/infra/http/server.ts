9a0f541864eb37f625e9040563b8ccc34211849d
lucasb <lkab05@hotmail.com>
2025-09-09 19:10:00 -0300

    feat: assembled server

--- src/infra/http/server.ts
+++ src/infra/http/server.ts
@@ -11,6 +11,7 @@ import {
   validatorCompiler,
 } from 'fastify-type-provider-zod';
 import { uploadImageRoute } from './routes/upload-image';
+import { transformSwaggerSchema } from './transform-swagger-schema';
 
 const server = fastify();
 
@@ -39,7 +40,7 @@ server.register(fastifySwagger, {
       version: '1.0.0',
     },
   },
-  transform: jsonSchemaTransform,
+  transform: transformSwaggerSchema,
 });
 
 server.register(fastifySwaggerUi, {
