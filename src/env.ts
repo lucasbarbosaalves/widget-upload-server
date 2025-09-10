c731eda4a8a088014606809465b32e089c39974d
lucasb <lkab05@hotmail.com>
2025-09-10 14:13:02 -0300

    feat: added Cloudflare R2 env variables to project

--- src/env.ts
+++ src/env.ts
@@ -4,6 +4,11 @@ const envSchema = z.object({
   PORT: z.coerce.number().default(3000),
   NODE_ENV: z.enum(['development', 'test', 'production']),
   DATABASE_URL: z.url().startsWith('postgresql://'),
+  CLOUDFLARE_ACCOUNT_ID: z.string(),
+  CLOUDFLARE_ACCESS_KEY_ID: z.string(),
+  CLOUDFLARE_SECRET_ACCESS_KEY: z.string(),
+  CLOUFLARE_BUCKET: z.string(),
+  CLOUFLARE_PUBLIC_URL: z.url(),
 });
 
 export const env = envSchema.parse(process.env);
